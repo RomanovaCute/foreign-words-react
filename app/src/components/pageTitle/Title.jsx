25a149accacc892040c3af5cf060d04797fc2d76
RomanovaCute <romanova17romanova@yandex.ru>
2022-08-28 15:59:42 +0500

    refactor: rename TitleBox & change padding

--- app/src/components/pageTitle/Title.jsx
+++ app/src/components/pageTitle/Title.jsx
@@ -1,21 +1,30 @@
 import styled from 'styled-components'
 import ContextualExample from '../progressBar/Progress'
 
-const TitleBox = styled.div`
+const TitleWrapper = styled.div`
     display: flex;
+    flex-direction: column;
+    margin-bottom: 20px;
     justify-content: flex-start;
-    padding:0 40px;
+    padding:0 50px;
     font-family: 'Vag world bold';
     color: #04064f;
     font-size: 20px;
 `
+const TitleBox = styled.div`
+    display: flex;
+    justify-content; flex-start;
+    padding: 20px 0;
+`
 
 function Title(props){
     return(
-        <TitleBox>
-            <h2 className='title'>{props.title}</h2>
+        <TitleWrapper>
+            <TitleBox>
+                <h2 className='title'>{props.title}</h2>
+            </TitleBox>
             <ContextualExample></ContextualExample>
-        </TitleBox>
+        </TitleWrapper>
     )
 }
 //здесь еще должен быть прогрессбар
