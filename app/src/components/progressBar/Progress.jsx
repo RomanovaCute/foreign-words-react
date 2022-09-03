ac667813e4ec94c706194fb6d2742f33a3e241b6
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 23:58:21 +0500

    fix: add styles for ProgressBar

--- app/src/components/progressBar/Progress.jsx
+++ app/src/components/progressBar/Progress.jsx
@@ -1,7 +1,12 @@
 import ProgressBar from "@ramonak/react-progress-bar";
 
-const ProgressLine = () => {
-  return <ProgressBar completed={60} />;
+const ProgressLine = (props) => {
+  return <ProgressBar 
+        completed={props.completed}
+        maxCompleted={props.maxCompleted}
+        height='15px'
+        labelSize='14px'
+        />;
 };
   
-  export default ProgressLine;
+export default ProgressLine;
