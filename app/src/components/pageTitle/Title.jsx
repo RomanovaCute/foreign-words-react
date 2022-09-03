df530f5c2475410f31e6cd6f425590607c6d6bdb
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 21:14:14 +0500

    refactor: moved styles to 'styles.jsx'

--- app/src/components/pageTitle/Title.jsx
+++ app/src/components/pageTitle/Title.jsx
@@ -1,21 +1,5 @@
-import styled from 'styled-components'
 import ContextualExample from '../progressBar/Progress'
-
-const TitleWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    margin-bottom: 20px;
-    justify-content: flex-start;
-    padding:0 50px;
-    font-family: 'Vag world bold';
-    color: #04064f;
-    font-size: 20px;
-`
-const TitleBox = styled.div`
-    display: flex;
-    justify-content; flex-start;
-    padding: 20px 0;
-`
+import {TitleWrapper, TitleBox} from './styles'
 
 function Title(props){
     return(
@@ -27,6 +11,5 @@ function Title(props){
         </TitleWrapper>
     )
 }
-//здесь еще должен быть прогрессбар
 
 export default Title;
---
+++ app/src/components/pageTitle/styles.jsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components'
+
+const TitleWrapper = styled.div`
+display: flex;
+flex-direction: column;
+margin-bottom: 20px;
+justify-content: flex-start;
+padding:0 50px;
+font-family: 'Vag world bold';
+color: #04064f;
+font-size: 20px;
+`
+const TitleBox = styled.div`
+display: flex;
+justify-content; flex-start;
+padding: 20px 0;
+`
+
+export {TitleWrapper, TitleBox};
