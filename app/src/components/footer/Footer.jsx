530b2a81486482883ea4a12eb796ea0dfcaf1b81
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 21:13:36 +0500

    refactor: moved styles to 'styles.jsx'

--- app/src/components/footer/Footer.jsx
+++ app/src/components/footer/Footer.jsx
@@ -1,28 +1,5 @@
-import styled from 'styled-components'
 import logo from '../../images/logo.png'
-
-const FooterBox = styled.div`
-    flex-direction: column;
-    // position: fixed;
-    // bottom: 0;
-    // width: 100%;
-    align-items: center;
-    background: #c5e8fc;
-    bottom: 0;
-    font-family: 'Vag world bold';
-    color: #04064f;
-`
-const FooterWrapper = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding: 10px 40px;
-
-    img{
-        width: 2%;
-        border-radius: 10px;
-    }
-`
+import  {FooterBox, FooterWrapper} from './styles'
 
 function Footer(){
     return (
