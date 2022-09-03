3d3b9a1008670972133336023632c020f545b12b
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 21:14:05 +0500

    refactor: moved styles to 'styles.jsx'

--- app/src/components/navigation/nav.jsx
+++ app/src/components/navigation/nav.jsx
@@ -1,45 +1,5 @@
-import styled from 'styled-components'
 import logo from '../../images/logo.png'
-
-const CustomNav = styled.div`
-    display: flex;
-    flex-direction: row;
-
-    font-family: 'Vag world bold';
-    color: #04064f;
-    font-weight: bold;
-    font-size: 18px;
-    box-shadow: 5px 1px 50px rgb(167 167 167 / 50%);
-`
-
-const Wrapper = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    padding: 10px 40px;
-    width: 100%;
-`
-
-const LogoBox = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    grid-gap: 10px;
-
-    img{
-        width: 7%;
-    }
-`
-
-const Items = styled.div`
-    display: flex;
-    flex-direction: row;
-    gap: 20px;
-
-    .study,.dictionary{
-        cursor: pointer;
-    }
-`
+import {CustomNav, Wrapper, LogoBox, Items} from './styles'
 
 function Nav(){
     return (
---
+++ app/src/components/navigation/styles.jsx
@@ -0,0 +1,42 @@
+import styled from 'styled-components'
+
+const CustomNav = styled.div`
+    display: flex;
+    flex-direction: row;
+    font-family: 'Vag world bold';
+    color: #04064f;
+    font-weight: bold;
+    font-size: 18px;
+    box-shadow: 5px 1px 50px rgb(167 167 167 / 50%);
+`
+
+const Wrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 10px 40px;
+    width: 100%;
+`
+
+const LogoBox = styled.div`
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    grid-gap: 10px;
+
+    img{
+        width: 7%;
+    }
+`
+
+const Items = styled.div`
+    display: flex;
+    flex-direction: row;
+    gap: 20px;
+
+    .study,.dictionary{
+        cursor: pointer;
+    }
+`
+
+export {CustomNav, Wrapper, LogoBox, Items};
