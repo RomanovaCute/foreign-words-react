5752e9192d872710c064e20c957b4823ec10fe4b
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 19:34:53 +0500

    feat: add Slider & styles

---
+++ app/src/components/slider/Slider.jsx
@@ -0,0 +1,56 @@
+import nextButton from '../../images/next.png'
+import prevButton from '../../images/back.png'
+import React, { useState } from 'react';
+import Card from '../card/Card';
+import {Wrapper, Container, Button, CardBox} from './styles'
+
+
+function Slider({words}){
+    const [slideIndex, setSlideIndex] = useState(1);
+
+    const prevCard = () => {
+        if(slideIndex !== words.length){
+            setSlideIndex(slideIndex + 1)
+        } else if (slideIndex === words.length){
+            setSlideIndex(1)
+        }
+    }
+
+    const nextCard = () => {
+        if(slideIndex !== 1){
+            setSlideIndex(slideIndex - 1)
+        } else if (slideIndex === 1) {
+            setSlideIndex(words.length);
+          }
+    }
+    
+
+    const wordsItem = words.map((item) => {
+        const {id, ...wordProps} = item;
+
+        return (
+            <Card key={id}
+            {...wordProps}/>
+        )
+            
+    })
+
+    return(
+        <Wrapper>
+            <Container>
+                <Button onClick={prevCard}>
+                    <img src={prevButton}></img>
+                </Button>
+                <CardBox>
+                    {wordsItem[slideIndex - 1]}
+                </CardBox>
+                <Button onClick={nextCard}>
+                    <img src={nextButton}></img>
+                </Button>
+            </Container>
+            <div className="counter">Изучено слов: </div>
+        </Wrapper>
+    )
+}
+
+export default Slider;
---
+++ app/src/components/slider/styles.jsx
@@ -0,0 +1,35 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 30px;
+`
+
+const Container = styled.div`
+    display: flex;
+    flex-direction: row;
+    margin: 0 auto;
+    width: 100%;
+    align-items: center;
+    justify-content: center;
+`
+const Button = styled.button`
+    width: 20%;
+    height: 20%;
+    border: none;
+    background: none;
+    cursor: pointer;
+
+    img{
+        width: 20%;
+    }
+`
+
+const CardBox = styled.div`
+    display: flex;
+    width: 30%;
+    justify-content: center;
+`
+
+export {Wrapper, Container, Button, CardBox};
