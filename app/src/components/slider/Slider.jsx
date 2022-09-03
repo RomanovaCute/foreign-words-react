2b50a5aa7d9558976b705524b87e3f35e6ec72c1
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-03 23:58:44 +0500

    feat: add counter

--- app/src/components/slider/Slider.jsx
+++ app/src/components/slider/Slider.jsx
@@ -2,13 +2,17 @@ import nextButton from '../../images/next.png'
 import prevButton from '../../images/back.png'
 import React, { useState } from 'react';
 import Card from '../card/Card';
-import {Wrapper, Container, Button, CardBox} from './styles'
+import ProgressBar from '../progressBar/Progress'
+import {Wrapper, Container, Button, CardBox, ProgressBox, CounterBox} from './styles'
 
 
 function Slider({words}){
     const [slideIndex, setSlideIndex] = useState(1);
+    const [wordCount, setwordCount] = useState(0);
+    const [wordLearned, setwordLearned] = useState([]);
 
-    const prevCard = () => {
+    
+    const nextCard = () => {
         if(slideIndex !== words.length){
             setSlideIndex(slideIndex + 1)
         } else if (slideIndex === words.length){
@@ -16,27 +20,56 @@ function Slider({words}){
         }
     }
 
-    const nextCard = () => {
+    const prevCard = () => {
         if(slideIndex !== 1){
             setSlideIndex(slideIndex - 1)
         } else if (slideIndex === 1) {
             setSlideIndex(words.length);
-          }
+        }
     }
     
+   const knownWords = (id) => {
+    console.log(id);
+        let arr = [...wordLearned];
+        arr.push(id);
+
+        let result = [];
+        for(let num of arr){
+            if(!result.includes(num)){
+                result.push(num);
+            }
+        }
+        setwordLearned(result);
+        setwordCount(result.length)
+    };
 
     const wordsItem = words.map((item) => {
         const {id, ...wordProps} = item;
 
         return (
-            <Card key={id}
-            {...wordProps}/>
-        )
-            
+            <Card
+                key={id}
+                id={id}
+                knownWords={knownWords}
+                {...wordProps}
+            />
+        )      
     })
 
+    
     return(
         <Wrapper>
+            <ProgressBox>
+                <CounterBox>
+                    <span>Progress</span>
+                    <div className='counter'>
+                        <span className='current-word'>{wordCount} </span> 
+                        <span className='total-words'>/ {words.length}</span>
+                    </div>
+                </CounterBox>
+                <ProgressBar completed={wordCount} maxCompleted={words.length}></ProgressBar>
+            </ProgressBox>
+            
             <Container>
                 <Button className='prev' onClick={prevCard}>
                     <img className='prev-arrow' src={prevButton}></img>
@@ -48,7 +81,7 @@ function Slider({words}){
                     <img className='next-arrow' src={nextButton}></img>
                 </Button>
             </Container>
-            <div className="counter">Изучено слов: </div>
+            
         </Wrapper>
     )
 }
