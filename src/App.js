cc2f0623d0b4cd8b43e5b9534fc09b66c6f0e876
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-19 15:53:53 +0500

    feat: add routing

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import {
   Routes,
   Route
 } from "react-router-dom";
-import { useEffect } from "react";
 import './styles/App.css';
 import Nav from './components/navigation/Nav'
 import Footer from './components/footer/Footer'
-import Homepage from "./components/pages/homepage/Homepage";
-import Studypage from "./components/pages/studypage/Studypage";
+import Homepage from "./components/pages/homepage/Home";
+import Studypage from "./components/pages/studypage/Study";
 import Dictionarypage from "./components/pages/dictionarypage/Dictionary";
+import NotFoundpage from "./components/pages/notfoundpage/NotFound";
 
 
 
@@ -18,11 +18,14 @@ function App() {
     <Router>
     <div className="App">
       <Nav/>
+      <div className="main">
         <Routes>
           <Route exact path="/" element={<Homepage />} />
-          <Route path="/study" element={<Studypage />}/>
+          <Route path="/study" element={<Studypage/>}/>
           <Route path="/dictionary" element={<Dictionarypage/>} />
+          <Route path="*" element={<NotFoundpage/>} />
         </Routes>
+      </div>
       <Footer/>
     </div>
     </Router>
