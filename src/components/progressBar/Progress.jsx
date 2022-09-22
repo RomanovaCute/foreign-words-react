2f68629a1169ec6a81427f8057460556a77e5337
RomanovaCute <romanova17romanova@yandex.ru>
2022-09-22 19:34:55 +0500

    fix progressbar & add result-image

--- src/components/progressBar/Progress.jsx
+++ src/components/progressBar/Progress.jsx
@@ -1,9 +1,10 @@
 import ProgressBar from "@ramonak/react-progress-bar";
-import { words } from "../../sources/wordsStore";
+// import { words } from "../../sources/wordsStore";
 
 const ProgressLine = (props) => {
   return <ProgressBar 
-        completed={Math.round((props.completed * 100 )/(words.length))}
+        // completed={Math.round((props.completed * 100 )/(words.length))}
+        completed={Math.round((props.completed * 100 )/(15))}
         maxCompleted={ 100 }
         height='15px'
         labelSize='14px'
